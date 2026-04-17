01c72b189b4f8c0589d5eefa35f6a3acb335ad3e
Barasa-Paul-Joseph21 <barasapjoseph.codes@gmail.com>
2026-04-17 09:11:26 +0300

    Initial commit - Spring Boot setup with H2 Database

---
+++ student-results-api/src/test/java/student_results_api/StudentResultsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package student_results_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentResultsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
